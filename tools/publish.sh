7332a266497a724a28aaf20195a1489b35c0069f
ericleimar <e.a.leimar@gmail.com>
2017-03-30 23:06:09 +0200

    changed publish script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d "public/.git" ]; then
-	git clone --depth 1 --single-branch https://github.com/ukmc-web/ukmc.se -b gh-pages public
+	git clone --depth 1 --single-branch https://github.com/ukmc-web/ukmc.se -b gh-pages ../ukmc-web/public
 fi
 rm -rf public/*
 hugo
